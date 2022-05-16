91bf03fbe4182ec7be9456ae72045ff90b87b72e
Daniel Rothwell <danielrothwell@Daniels-MacBook-Pro.local>
2022-05-16 17:35:19 -0400

    hello

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/apples', (req, res) => {
+  res.status(200).send('Dan Rothwell!');
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.status(418).send('Teapots only');
+});
 
+app.get('*', (req, res) => {
+  res.status(404).send('Nothing here!');
+});
+
+
+
+// app.get('*', (req, res) => {
+//   res.send('Dan Rothwell');
+// });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
+//"browser":"open -a Safari http://localhost:3000"
